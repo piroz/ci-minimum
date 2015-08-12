236a7c7b3604b5d752411985ce736c1ee0a940c7
Hiroshi Murakami <piroz@users.noreply.github.com>
2015-08-12 19:18:16 +0900

    基本定数 ON OFF フラグ

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,14 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/**
+ * フラグon
+ */
+define('FLAG_ON', '1');
+/**
+ * フラグoff
+ */
+define('FLAG_OFF', '0');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
