bc9110d5349ec7b4e2fe6d07c5d1c591147ac46b
Hiroshi Murakami <piroz@users.noreply.github.com>
2015-08-23 19:15:04 +0900

    update MY_Input add Method isGet isPut isDelete

--- application/core/MY_Input.php
+++ application/core/MY_Input.php
@@ -8,6 +8,21 @@ if (!defined('BASEPATH'))
  */
 class MY_Input extends CI_Input
 {
+    const SERVER_KEY_METHOD = 'REQUEST_METHOD';
+    const METHOD_STR_POST = 'POST';
+    const METHOD_STR_GET = 'GET';
+    const METHOD_STR_PUT = 'PUT';
+    const METHOD_STR_DELETE = 'DELETE';
+    
+    /**
+     * method
+     * 
+     * @return string
+     */
+    public function method()
+    {
+        return strtoupper($this->server(self::SERVER_KEY_METHOD));
+    }
 
     /**
      * isPost
@@ -18,7 +33,42 @@ class MY_Input extends CI_Input
      */
     public function isPost()
     {
-        return strtolower($this->server('REQUEST_METHOD')) === 'post';
+        return $this->method() === self::METHOD_STR_POST;
     }
 
+    /**
+     * isGet
+     * 
+     * GETリクエストであったかどうか？
+     * 
+     * @return boolean
+     */
+    public function isGet()
+    {
+        return $this->method() === self::METHOD_STR_GET;
+    }
+    
+    /**
+     * isPut
+     * 
+     * PUTリクエストであったかどうか？
+     * 
+     * @return boolean
+     */
+    public function isPut()
+    {
+        return $this->method() === self::METHOD_STR_PUT;
+    }
+    
+    /**
+     * isDelete
+     * 
+     * DELETEリクエストであったかどうか？
+     * 
+     * @return boolean
+     */
+    public function isDelete()
+    {
+        return $this->method() === self::METHOD_STR_DELETE;
+    }
 }
